b51e92c0929653d808694d24aec7d67212e9cec3
johnsjoo <70665563+johnsjoo@users.noreply.github.com>
2021-05-18 15:54:57 +0200

    Initial commit

---
+++ VAU-CoreApi/Data/Context.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Areas.Identity.Data
+{
+    public class Context : IdentityDbContext<User>
+    {
+
+        public DbSet<UserSettings> UserSettings { get; set; }
+        public DbSet<UserGDPR> UserGDPR { get; set; }
+
+
+        public Context(DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            const string adminId = "admin-c0-aa65-4af8-bd17-00bd9344e575";
+            const string roleId = "root-0c0-aa65-4af8-bd17-00bd9344e575";
+            const string userRoleId = "user-2c0-aa65-4af8-bd17-00bd9344e575";
+
+            builder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Id = roleId,
+                Name = "root",
+                NormalizedName = "ROOT"
+            });
+
+            builder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Id = userRoleId,
+                Name = "User",
+                NormalizedName = "USER"
+            });
+
+            var hasher = new PasswordHasher<User>();
+
+            builder.Entity<User>().HasData(new User
+            {
+                Id = adminId,
+                UserName = "admin",
+                NormalizedUserName = "ADMIN",
+                Email = "admin@core.api",
+                NormalizedEmail = "ADMIN@CORE.API",
+                EmailConfirmed = true,
+                PasswordHash = hasher.HashPassword(null, "AdminPass1!"),
+                SecurityStamp = Guid.NewGuid().ToString(),
+                // MyProperty = 13
+            });
+
+            builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
+            {
+                RoleId = roleId,
+                UserId = adminId
+            });
+        }
+
+        
+    }
+}
