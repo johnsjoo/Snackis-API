cb9405c7f30396a87e7e4124e7a2e5e13251870f
Azamat Bayev <azamat.bayev1@gmail.com>
2021-06-10 16:04:34 +0200

    Added controller messages
    Added controller for sending and getting messages as user. Backend part is done. :)

---
+++ Snackis -API/Data/Message.cs
@@ -0,0 +1,29 @@
+﻿using Api.Areas.Identity.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Data
+{
+    public class Message
+    {
+        [Key]
+        public string Id { get; set; }
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        public string message { get; set; }
+        public DateTime Date { get; set; }
+        public string MessageReceiver { get; set; }
+        public virtual User User { get; set; }
+
+
+
+        public Message()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ Snackis -API/Models/MessageModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Api.Controllers
+{
+    public class MessageModel
+    {
+        public string UserId { get; set; }
+        public string message { get; set; }
+        public DateTime Date { get; set; }
+        public string MessageReceiver { get; set; }
+    }
+}
